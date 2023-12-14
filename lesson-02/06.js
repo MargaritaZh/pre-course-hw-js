6a95bd5ee1e6518d9414f1ce742dbdfba4a2c5d9
MargaritaZh <margaritazhitkevich@gmail.com>
2023-12-14 23:21:17 +0300

    задача 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -7,6 +7,20 @@ let passportWithAddress = {
   },
 };
 
+function deepCopy(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => deepCopy(item));
+  }
+  if (typeof obj === "object" && obj !== null) {
+    const newObj = {};
+    for (let key in obj) {
+      newObj[key] = deepCopy(obj[key]);
+    }
+    return newObj;
+  }
+  return obj;
+}
+
 let passportWithAddress1 = deepCopy(passportWithAddress);
 passportWithAddress1.address.city = "Bobryisk";
 console.log(passportWithAddress.address.city);
