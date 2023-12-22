435d8fcf0f414e79f3f7d503fb0bf3eb6aaf20a9
MargaritaZh <margaritazhitkevich@gmail.com>
2023-12-22 23:30:41 +0300

    задача 7

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -1,7 +1,7 @@
-​const numbers = [
+const numbers = [
   [1, 2, 3, 4, 5],
   [11, 20, 33, 40, 55],
   [111, 200, 333, 400, 555],
 ];
-let newArr=numbers.map(row=>row.filter(num=>num%2===0));
+let newArr = numbers.map((row) => row.filter((num) => num % 2 === 0));
 console.log(newArr);
