3028595203339ed5b474b8cde70706e39dac8405
MargaritaZh <margaritazhitkevich@gmail.com>
2023-12-22 22:12:15 +0300

    задача 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString(arr) {
+  let newColor = arr
+    .filter((el) => el === "черный" || el === "красный" || el === "желтый")
+    .join("-");
+  return newColor;
+}
+
+console.log(createColorString(colors));
